2226a51d7549f7f8b54a28873e9160fa56ae65f6
Roqia <roqia5423@gmail.com>
2023-03-16 15:00:51 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long lg;
 long long int x;
 float f;
-printf("Size of a char: %c byte(s)\n", sizeof(c));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
 printf("Size of an int: %d byte(s)\n", sizeof(i));
 printf("Size of a long int: %d byte(s)\n", sizeof(lg));
 printf("Size of a long long int: %d byte(s)\n", sizeof(x));
-printf("Size of a long float: %f byte(s)\n", sizeof(f));
+printf("Size of a float: %f byte(s)\n", sizeof(f));
 return (0);
 }
