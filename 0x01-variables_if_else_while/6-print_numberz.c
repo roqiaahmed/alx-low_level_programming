530f1282a2b760728c9fed74c472446d565e88bd
Roqia <roqia5423@gmail.com>
2023-03-17 07:26:33 -0700

    9-print_comb

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,10 @@
 int main(void)
 {
 int num;
-for(num='0'; num<='9'; num++)
+for (num = '0'; num <= '9'; num++)
 {
 putchar(num);
 };
 putchar('\n');
-return(0);
+return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,14 +7,14 @@
 int main(void)
 {
 char ch;
-for(ch='0'; ch<='9'; ch++)
+for (ch = '0'; ch <= '9'; ch++)
 {
 putchar(ch);
 };
-for(ch='a'; ch<='f'; ch++)
+for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
 };
 putchar('\n');
-return(0);
+return (0);
 }
