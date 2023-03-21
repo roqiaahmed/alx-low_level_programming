237b60acadb1f0feeef83cdd071f03eff4dbb390
Roqia <roqia5423@gmail.com>
2023-03-21 08:57:03 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-print_alphabet();
-return (0);
+    print_alphabet();
+    return (0);
 }
