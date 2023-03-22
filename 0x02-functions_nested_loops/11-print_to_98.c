a4abe3f377f925441ab513d3c00fbabaa9c72040
Roqia <roqia5423@gmail.com>
2023-03-21 17:12:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,18 +5,35 @@
  */
 void print_to_98(int n)
 {
-int i;
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+};
 if (n > 98)
 {
-for (i = n; i >= 98; i--)
-printf("%d, ", i);
+for (; n >= 98; n--)
+{
+if (n != 98)
+printf("%d, ", n);	
+else
+{
+printf("%d", n);
+printf("\n");
+};
+};
 };
 if (n < 98)
 {
-for (i = n; i <= 98; i++)
-printf("%d, ", i);
-};
-if (n == 98)
+for (; n <= 98; n++)
+{
+if (n != 98)
 printf("%d, ", n);
+else
+{
+printf("%d", n);
 printf("\n");
+};      
+};
+};
 }
