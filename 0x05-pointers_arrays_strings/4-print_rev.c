3082c3e1e720472e93f0afdd188ea1a5bc3656b3
Roqia <roqia5423@gmail.com>
2023-03-27 12:56:29 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+ /**
+  * print_rev - prints a string, in reverse
+  *
+  * @s: The character to print
+  */
+void print_rev(char *s)
+{
+
+    int i = 0;
+    int n;
+    n = strlen(s);
+    
+    for (i = n; i >= 0; i--)
+    {
+        char test = s[i];
+        _putchar(test);
+    }
+    _putchar('\n');
+}
