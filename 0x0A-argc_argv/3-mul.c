820e0f1b444aa6ab6d7b983832b5fe29da89da50
Roqia <roqia5423@gmail.com>
2023-04-09 16:25:52 -0700

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,10 +14,13 @@ int i, mul = 1;
 if (argc > 1)
 {
 for (i = 1; i < argc; i++)
-mul = mul * atoi(argv[i]);
+mul = mul *atoi(argv[i]);
 printf("%d\n", mul);
-return (0);
 }
+else
+{
 printf("Error\n");
 return (1);
+}
+return (0);
 }
