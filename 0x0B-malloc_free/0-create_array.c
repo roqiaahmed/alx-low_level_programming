d45e25bd764ba6ce59ef7ae206c5321f99746901
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-04-12 21:06:34 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,8 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
-char *ptr = malloc(size);
-if (size == 0)
+char *ptr = malloc(sizeof(char) * size);
+if (size == 0 || ptr == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 ptr[i] = c;
