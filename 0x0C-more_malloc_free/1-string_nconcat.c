0f02c3419e49bb05507704d8bc9d962134be55bd
Roqia <roqia5423@gmail.com>
2023-04-13 17:49:12 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,47 +1,90 @@
-#include "main.h"
 #include <stdlib.h>
+		
+#include "main.h"
+		
+
+		
 /**
- * _size - check the number of stirng
- *@str: the stirng
+		
+ * *string_nconcat - concatenates n bytes of a string to another string
+		
+ * @s1: string to append to
+		
+ * @s2: string to concatenate from
+		
+ * @n: number of bytes from s2 to concatenate to s1
+		
  *
- * Return: number.
+		
+ * Return: pointer to the resulting string
+		
  */
-int _size(char *str)
-{
-int i = 0;
-while (str[i] != '\0')
-i++;
-return (i);
-}
-/**
- * string_nconcat - concatenates two strings.
- *@s1: the stirng one
- *@s2: the stirng two
- *@n: size of s2
- *
- * Return: concatenates.
- */
+		
 char *string_nconcat(char *s1, char *s2, unsigned int n)
+		
 {
-int i;
-unsigned int j;
-unsigned int len = 0;
-char *ptr;
-while (s2[len] != '\0')
-len++;
-if (n < len)
-ptr = malloc(sizeof(char) * (_size(s1) + n + 1));
-else
-ptr = malloc(sizeof(char) * (_size(s1) + _size(s2) + 1));
-if (ptr == NULL)
-return (NULL);
-for (i = 0; i < _size(s1); i++)
-ptr[i] = s1[i];
-if (n < len)
-for (j = 0; j < n; j++)
-ptr[_size(s1) + j] = s2[j];
-else
-for (j = 0; j < len; j++)
-ptr[_size(s1) + j] = s2[j];
-return (ptr);
+		
+	char *s;
+		
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+		
+
+		
+	while (s1 && s1[len1])
+		
+		len1++;
+		
+	while (s2 && s2[len2])
+		
+		len2++;
+		
+
+		
+	if (n < len2)
+		
+		s = malloc(sizeof(char) * (len1 + n + 1));
+		
+	else
+		
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		
+
+		
+	if (!s)
+		
+		return (NULL);
+		
+
+		
+	while (i < len1)
+		
+	{
+		
+		s[i] = s1[i];
+		
+		i++;
+		
+	}
+		
+
+		
+	while (n < len2 && i < (len1 + n))
+		
+		s[i++] = s2[j++];
+		
+
+		
+	while (n >= len2 && i < (len1 + len2))
+		
+		s[i++] = s2[j++];
+		
+
+		
+	s[i] = '\0';
+		
+
+		
+	return (s);
+		
 }
+
