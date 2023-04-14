cf84b60f9c9904c26e6f4572a86649c6b1d7a608
Roqia <roqia5423@gmail.com>
2023-04-13 18:28:11 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned int i;
 for (i = 0; i < n; i++)
 {
 s[i] = b;
@@ -26,7 +27,6 @@ return (s);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i;
 char *ptr;
 if (nmemb == 0 || size == 0)
 return (NULL);
