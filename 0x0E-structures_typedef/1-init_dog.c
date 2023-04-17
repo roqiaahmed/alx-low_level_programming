e03b09ee8aef9302821fade2fadf8f58ff6ada71
Roqia <roqia5423@gmail.com>
2023-04-17 14:32:49 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initialize a variable
+ *@d: struct
+ *@name: the name
+ *@age: the age
+ *@owner: the owner
+ *
+ * Return: info of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ *@d: struct
+ *
+ * Return: info of dog
+ */
+
+void print_dog(struct dog *d)
+{
+if ((*d).name == NULL)
+printf("Name: (nil)\n");
+else
+printf("Name: %s\n", (*d).name);
+if ((*d).age == 0)
+printf("age: (nil)\n");
+else
+printf("age: %f\n", (*d).age);
+if ((*d).owner == NULL)
+printf("owner: (nil)\n");
+else
+printf("owner: %s\n", (*d).owner);
+}
