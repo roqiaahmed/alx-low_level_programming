869e2583088ef4d76b6d5357265b869fdd406b45
Roqia <roqia5423@gmail.com>
2023-04-19 16:13:18 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+char *s;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+s = argv[2];
+if ((*s == '/' && b == 0) ||
+(*s == '%' && b == 0))
+{
+printf("Error\n");
+exit(100);
+}
+if (get_op_func(s) == NULL || s[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+printf("%d\n", get_op_func(s)(a, b));
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - addition a,b
+ * @a: frist num
+ * @b: suc num
+ *
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+ /**
+ * op_sub - sub a,b
+ * @a: frist num
+ * @b: suc num
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+ /**
+ * op_mul - mul a,b
+ * @a: frist num
+ * @b: suc num
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+ /**
+ * op_div - div a,b
+ * @a: frist num
+ * @b: suc num
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+ /**
+ * op_mod - mod a,b
+ * @a: frist num
+ * @b: suc num
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
