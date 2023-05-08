709cbde99573c3cef809a2e90a211e96b497b844
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-05-09 01:03:30 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,15 +13,15 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd, len = 0;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_ APPEND);
-if (fd == -1)
-return (-1);
 if (text_content != NULL)
 {
 while (text_content[len] != '\0')
 len++;
 }
+fd = open(filename, O_WRONLY | O_APPEND);
 write(fd, text_content, len);
+if (fd == -1)
+return (-1);
 close(fd);
 return (1);
 }
