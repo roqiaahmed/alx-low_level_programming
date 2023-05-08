43e8ee854317965b0c85edf33ec60677315738f6
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-05-08 14:57:00 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fd, len = 0;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_WRONLY | O_ APPEND)
+fd = open(filename, O_WRONLY | O_ APPEND);
 if (fd == -1)
 return (-1);
 if (text_content != NULL)
