f70f3e3fdce24243e42ccfe002750ad4f8d51bc7
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-06-16 06:25:58 -0700

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,35 @@
 #include "lists.h"
 /**
+ * get_dnodeint_at_index -  returns the nth node
+ * @head: head of the list
+ * @index: the index
+ *
+ * Return: the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *current = head;
+unsigned int i = 0;
+unsigned int num = 0;
+const dlistint_t *curr = head;
+while (curr != NULL)
+{
+curr = curr->next;
+num += 1;
+}
+if (num - 1 < index)
+return (NULL);
+else
+{
+while (i != index)
+{
+current = current->next;
+i++;
+}
+return (current);
+}
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @n: integer
  * @idx: the index of the list
