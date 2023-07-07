9b9a0461e0bb1dd5cb44eab4e31ee5b8ed001c10
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-07-07 20:50:34 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,26 +8,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    unsigned long int i;
-    hash_table_t *table = NULL;
-    hash_node_t **array = NULL;
-    table = (hash_table_t *)malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return NULL;
-    }
-    array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
-    if (array == NULL)
-    {
-        free(table);
-        return NULL;
-    }
-    for (i = 0; i < size; i++)
-    {
-        array[i] = NULL;
-    }
-
-    table->size = size;
-    table->array = array;
-    return (table);
+unsigned long int i;
+hash_table_t *table = NULL;
+hash_node_t **array = NULL;
+table = (hash_table_t *)malloc(sizeof(hash_table_t));
+if (table == NULL)
+{
+return NULL;
+}
+array = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+if (array == NULL)
+{
+free(table);
+return NULL;
+}
+for (i = 0; i < size; i++)
+{
+array[i] = NULL;
+}
+table->size = size;
+table->array = array;
+return (table);
 }
