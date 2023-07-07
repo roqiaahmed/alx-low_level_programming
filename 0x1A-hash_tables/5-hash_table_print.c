907bd3a38752a85288ac7ee15bb354f20f4c2f29
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2023-07-07 20:55:49 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,29 +9,26 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *current_node;
-    unsigned long int i;
-    int is_first = 1;
-
-    if (ht == NULL)
-        return;
-
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] != NULL)
-        {
-            current_node = ht->array[i];
-
-            while (current_node != NULL)
-            {
-                if (!is_first)
-                    printf(", ");
-                printf("'%s': '%s'", current_node->key, current_node->value);
-                is_first = 0;
-                current_node = current_node->next;
-            }
-        }
-    }
-    printf("}\n");
+hash_node_t *current_node;
+unsigned long int i;
+int is_first = 1;
+if (ht == NULL)
+return;
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+if (ht->array[i] != NULL)
+{
+current_node = ht->array[i];
+while (current_node != NULL)
+{
+if (!is_first)
+printf(", ");
+printf("'%s': '%s'", current_node->key, current_node->value);
+is_first = 0;
+current_node = current_node->next;
+}
+}
+}
+printf("}\n");
 }
