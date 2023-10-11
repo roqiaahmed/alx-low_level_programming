64d175f4c243f813248b143954e9d06bfa341cec
Roqia Ahmed <roqiaahmed447@gmail.com>
2023-10-11 14:27:21 +0300

    104-advanced_binary

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -2,6 +2,26 @@
 #include <stdio.h>
 
 /**
+ * list_arr - print the array
+ *
+ * @array:pointer to the first element
+ * @left:start of the  array
+ * @right:end of the  array
+ */
+void list_arr(int *array, int left, int right)
+{
+    int i;
+
+    printf("Searching in array: ");
+    for (i = left; i <= right; i++)
+    {
+        printf("%d", array[i]);
+        if (i != right)
+            printf(", ");
+    }
+    printf("\n");
+}
+/**
  * advanced_binary - searches for a value in an array
  *
  * @array: pointer to the first element
@@ -21,15 +41,7 @@ int advanced_binary(int *array, size_t size, int value)
     while (left <= right)
     {
         int mid = left + (right - left) / 2;
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i != right)
-                printf(", ");
-        }
-        printf("\n");
-        // list_arr(array, left, right);
+        list_arr(array, left, right);
         if (array[mid] == value)
         {
             if (mid == left || array[mid - 1] != value)
